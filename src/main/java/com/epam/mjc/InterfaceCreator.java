8cb9ceda0e5e587d309bb8feaf558ba7e51aba15
Максим <Maxim.d.morozov@gmail.com>
2023-07-12 23:56:38 +0400

    done

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,13 @@
 package com.epam.mjc;
 
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> list.stream()
+                .map(a -> a / divider)
+                .collect(Collectors.toList());
+
     }
 }
